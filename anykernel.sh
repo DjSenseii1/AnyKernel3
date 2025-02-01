8addc27a4b2f25054ac69a2b80ffbb60f225cb1f
Senseii <princekarlagas@gmail.com>
2025-02-01 12:00:02 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=AMD-Reborn+ by SenseiX
+kernel.string=N0lagKernel by SenseiiXD
 do.devicecheck=1
 do.modules=0
 do.systemless=1
